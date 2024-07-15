48c3dccddca82a9e85d861580db761b3a11369fa
Devansh Kapoor <devansh.kapoor@nashtechglobal.com>
2024-07-15 19:43:26 +0530

    fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,13 +21,13 @@ import { FileModule } from './fileUpload/file.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'multi_tenant_app',
-      // synchronize: process.env.NODE_ENV !== 'production', // Auto-sync in development, manual in production
-      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Adjust the path to your entities
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: true,
     }),
     TenantModule,
     LoginModule,
